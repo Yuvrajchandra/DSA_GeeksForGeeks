5c3794fb10034968b0d9cbec640c01fc9b74016a
Pushpit <pushpit07@gmail.com>
2021-03-12 15:44:19 +0530

    Create 10_Multiplication_Under_Modulo.cpp

---
+++ Mathematics/10_Multiplication_Under_Modulo.cpp
@@ -0,0 +1,41 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+
+int multiplicationUnderModulo(long long a,long long b)
+{
+    int M=1000000007;
+    return(((a % M) * (b % M)) % M);
+}
+
+// { Driver Code Starts.
+
+
+int main() {
+	int T;
+	
+	//taking testcases
+	cin>>T;
+	while(T--)
+	{
+	    long long a;
+	    long long b;
+	    
+	    //taking input a and b
+	    cin>>a>>b;
+	    
+	    //calling multiplicationUnderModulo() function
+	    cout<<multiplicationUnderModulo(a,b)<<endl;
+	}
+	return 0;
+}  // } Driver Code Ends

2b8edb06b268baee31c9a1d156819efb295cb009
Pushpit <pushpit07@gmail.com>
2021-05-06 13:55:52 +0530

    Create Binary to gray code.cpp

---
+++ Bit Magic/Binary to gray code.cpp
@@ -0,0 +1,33 @@
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    //  Function to find the gray code of given number n
+    int greyConverter(int n)
+    {
+        return n ^ n>>1;
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t,n;
+    cin>>t;//testcases
+    while(t--)
+    {
+        cin >> n;//input n
+        Solution ob;
+        //calling greyConverter() function
+        cout << ob.greyConverter(n) << endl;
+    }
+}
+  // } Driver Code Ends

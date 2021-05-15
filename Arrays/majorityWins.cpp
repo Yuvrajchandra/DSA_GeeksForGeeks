799402a5e139049ff750bec2d00736fd7fe32a51
Pushpit <pushpit07@gmail.com>
2021-05-15 19:08:32 +0530

    Create majorityWins.cpp

---
+++ Arrays/majorityWins.cpp
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+
+ // } Driver Code Ends
+//User function Template for C
+
+// Function to find element with more appearances between two elements in an
+// array.
+int majorityWins(int arr[], int n, int x, int y) {
+    int count_x = 0;
+    int count_y = 0;
+    
+    for(int i=0; i<n; i++) {
+        if (arr[i] == x)
+            count_x++;
+        else if (arr[i] == y)
+            count_y++;
+    }
+    
+    if (count_x == count_y) {
+        return (x<y) ? x : y;
+    }
+    else if (count_x > count_y)
+        return x;
+    else
+        return y;
+}
+
+// { Driver Code Starts.
+int main() {
+
+    int t; // Testcases
+    scanf("%d", &t); // Input the testcases
+    while (t--) // Until all testcases are exhausted
+    {
+        int n; // Size of array
+        scanf("%d", &n); // Input the size
+        int arr[n]; // Declaring array of size n
+        for (int i = 0; i < n; i++) // Running a loop to input all elements of arr
+            scanf("%d", &arr[i]);
+
+        int x, y; // declare x and y
+        scanf("%d%d", &x, &y); // input x and y
+        printf("%d\n", majorityWins(arr, n, x, y)); // calling the function that you complete
+    }
+
+    return 0;
+}

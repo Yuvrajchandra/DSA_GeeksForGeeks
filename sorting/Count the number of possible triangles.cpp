9f38728e599b9dcca8e2f58f753241560d2a83ae
Pushpit <pushpit07@gmail.com>
2021-06-07 13:56:56 +0530

    Create Count the number of possible triangles.cpp

---
+++ sorting/Count the number of possible triangles.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution
+{
+    public:
+    //Function to count the number of possible triangles.
+    int findNumberOfTriangles(int arr[], int n)
+    {
+        sort(arr, arr+n);
+     
+        int count = 0;
+     
+        for (int i = n - 1; i >= 1; i--) {
+            int l = 0, r = i - 1;
+            
+            while (l < r) {
+                if (arr[l] + arr[r] > arr[i]) {
+                    count += r - l;
+                    r--;
+                }
+                else
+                    l++;
+            }
+        }
+        
+        return count;
+    }
+};
+
+
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        int n;
+        cin>>n;
+        int arr[n];
+        for(int i=0; i<n; i++)
+            cin>>arr[i];
+        Solution ob;
+        cout<<ob.findNumberOfTriangles(arr,n) <<endl;
+    }
+    return 0;
+}

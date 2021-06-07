587cfd5242e90394896fd3a534985f67ff7e1e29
Pushpit <pushpit07@gmail.com>
2021-06-07 18:44:41 +0530

    Update Conversion_To_One.cpp

--- Contests/Arrays/Conversion_To_One.cpp
+++ Contests/Arrays/Conversion_To_One.cpp
@@ -40,46 +40,4 @@ int main()
         cout << conversionToOne(*it) << endl;
 
     return 0;
-}// C++ program for the above approach
-#include <bits/stdc++.h>
-using namespace std;
-
-int conversionToOne(int num)
-{
-	int count = 0;
-	
-	while (num > 1) {
-		count++;
-
-		if (num % 2 == 0)
-			num = num / 2;
-
-		else if (num % 4 == 1 || num==3)
-			num = num - 1;
-
-		else
-			num = num + 1;
-	}
-
-	return count;
-}
-
-
-int main()
-{
-    int T;
-    cin >> T;
-
-    vector<int> arr;
-
-    while (T--) {
-        int inp;
-        cin >> inp;
-        arr.push_back(inp);
-    }
-
-    for (auto it = arr.begin(); it != arr.end(); it++)
-        cout << conversionToOne(*it) << endl;
-
-    return 0;
 }

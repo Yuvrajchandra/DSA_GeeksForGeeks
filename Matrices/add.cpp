aece8361eda486677be529151898bacec1a48aa5
Pushpit <pushpit07@gmail.com>
2021-06-13 15:05:22 +0530

    Create add.cpp

---
+++ Matrices/add.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+
+
+class Solution
+{   
+    public:
+    //Function to add two matrices.
+    vector<vector<int> > sumMatrix( const vector<vector<int> >& A, const vector<vector<int> >& B)
+    {
+        vector<vector<int>> result;
+        
+        if(A.size() != B.size())
+            return result;
+        
+        for(int i=0; i<A.size(); i++) {
+            vector<int> res;
+            for(int j=0; j<B[i].size(); j++) {
+                if(A[i].size() != B[i].size())
+                    return result;
+                res.push_back( A[i][j] + B[i][j] );
+            }
+            result.push_back(res);
+        }
+        
+        return result;
+    }
+};
+
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--) 
+    {
+        int row, col;
+        cin>> row>> col;
+        vector<vector<int> > A(row); 
+        for(int i=0; i<row; i++)
+        {
+            A[i].assign(col, 0);
+            for( int j=0; j<col; j++)
+            {
+                cin>>A[i][j];
+            }
+        }
+        cin>> row>> col;
+        vector<vector<int> > B(row);
+        for(int i=0; i<row; i++)
+        {
+            B[i].assign(col, 0);
+            for( int j=0; j<col; j++)
+            {
+                cin>>B[i][j];
+            }
+        }
+        Solution ob;
+        vector<vector<int> > result = ob.sumMatrix(A,B);
+        if(result.size() == 0)
+            cout<<"-1";
+        else
+        {
+            for (int i = 0; i < result.size(); ++i)
+            {
+                for (int j = 0; j < result[0].size(); ++j)
+                {
+                    cout<<result[i][j]<<" ";
+                }
+            }
+        }
+        cout<<endl;
+    }
+    return 0;
+}

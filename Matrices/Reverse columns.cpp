b7b25a130fdeabfe161c9baeba34fc28067f15b4
Pushpit <pushpit07@gmail.com>
2021-06-15 14:32:11 +0530

    Create Reverse columns.cpp

---
+++ Matrices/Reverse columns.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+
+ // } Driver Code Ends
+
+
+class Solution
+{   
+    public:
+    //Function to reverse the columns of a matrix.
+    void swap(int* a, int* b)
+    {
+        int temp = *a;
+        *a = *b;
+        *b = temp;
+    }
+     
+    void reverseCol(vector<vector<int>> &matrix)
+    {
+        for (int i = 0; i < matrix.size(); i++) {
+            int start = 0;
+            int end = matrix[0].size() - 1;
+     
+            while (start < end) {
+                swap(&matrix[i][start], &matrix[i][end]);
+                start++;
+                end--;
+            }
+        }
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin>>t;
+    while(t--) 
+    {
+        int row, col;
+        cin>> row>> col;
+        vector<vector<int> > matrix(row); 
+        for(int i=0; i<row; i++)
+        {
+            matrix[i].assign(col, 0);
+            for( int j=0; j<col; j++)
+            {
+                cin>>matrix[i][j];
+            }
+        }
+        
+        Solution ob;
+        ob.reverseCol(matrix);
+
+        for (int i = 0; i < row; ++i)
+        {
+            for (int j = 0; j < col; ++j)
+            {
+                cout<<matrix[i][j]<<" ";
+            }
+        }
+        cout<<endl;
+    }
+    return 0;
+}

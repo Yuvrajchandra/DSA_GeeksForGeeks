b3fb716cff5331292b2c46b5e4860446a6dfdc41
Pushpit <pushpit07@gmail.com>
2021-06-15 13:41:42 +0530

    Create Exchange matrix columns.cpp

---
+++ Matrices/Exchange matrix columns.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+void exchangeColumns(vector<vector<int> > &matrix);
+
+ // } Driver Code Ends
+
+
+
+class Solution
+{
+    public:
+    //Function to exchange first column of a matrix with its last column.
+    void exchangeColumns(vector<vector<int> > &matrix)
+    {
+        for(int i=0; i<matrix.size(); i++) {
+            for(int j=0; j<matrix[0].size(); j++) {
+                if(j==0) {
+                    int temp = matrix[i][0];
+                    matrix[i][0] = matrix[i][(matrix[0].size() - 1)];
+                    matrix[i][(matrix[0].size() - 1)] = temp;
+                }
+            }
+        }
+    }
+};
+
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n1, m1;
+    	cin >> n1 >> m1;
+    	vector< vector<int>> matrix(n1, vector<int> (m1));
+    	for(int i = 0; i < n1; i++){
+    		for(int j = 0; j < m1; j++){
+    			cin >> matrix[i][j];
+    		}
+    	}
+    	Solution obj;
+    	obj.exchangeColumns(matrix);
+    	for(int i = 0; i < n1; i++){
+    		for(int j = 0; j < m1; j++){
+    			cout << matrix[i][j] << ' ';
+    		}
+    	}
+    	cout << endl;
+    }
+	return 0;
+}

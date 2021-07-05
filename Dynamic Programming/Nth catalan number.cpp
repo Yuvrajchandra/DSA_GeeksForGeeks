8e73247294b60250fe9e466ceccfef01f210be35
Pushpit <pushpit07@gmail.com>
2021-07-05 06:50:02 +0530

    Create Nth catalan number.cpp

---
+++ Dynamic Programming/Nth catalan number.cpp
@@ -0,0 +1,49 @@
+#include <boost/multiprecision/cpp_int.hpp>
+using boost::multiprecision::cpp_int;  // https://www.geeksforgeeks.org/factorial-large-number-using-boost-multiprecision-library/
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function template for C++
+
+
+class Solution
+{
+    public:
+    //Function to find the nth catalan number.
+    cpp_int findCatalan(int n) 
+    {
+        cpp_int catalan[n + 1];
+ 
+        // Initialize first two values in table
+        catalan[0] = catalan[1] = 1;
+     
+        for (int i = 2; i <= n; i++) {
+            catalan[i] = 0;
+            for (int j = 0; j < i; j++)
+                catalan[i] += catalan[j] * catalan[i - j - 1];
+        }
+     
+        // Return last entry
+        return catalan[n];
+    }
+};
+
+// { Driver Code Starts.
+
+int main() 
+{
+    //taking count of testcases
+	int t;
+	cin>>t;
+	while(t--) {
+	    
+	    //taking nth number
+	    int n;
+	    cin>>n;
+	    Solution obj;
+	    //calling function findCatalan function
+	    cout<< obj.findCatalan(n) <<"\n";    
+	}
+	return 0;
+}

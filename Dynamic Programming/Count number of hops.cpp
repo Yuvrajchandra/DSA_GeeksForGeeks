14e2be22b1efbb17848f7c75b118359afe83a584
Pushpit <pushpit07@gmail.com>
2021-07-05 07:27:11 +0530

    Update Count number of hops.cpp

--- Dynamic Programming/Count number of hops.cpp
+++ Dynamic Programming/Count number of hops.cpp
@@ -1,1 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+ // } Driver Code Ends
+
+
+
+class Solution
+{
+    public:
+    //Function to count the number of ways in which frog can reach the top.
+    long long countWays(int n)
+    {
+        long long res[n + 1];
+        long long temp = 0;
+        res[0] = 1;
+        res[1] = 1;
+        res[2] = 2;
+        for (int i = 3; i <= n; i++)
+            res[i] = (res[i - 1] + res[i - 2] + res[i - 3]) % 1000000007;
+ 
+        return res[n];
+    }
+};
+
+
+// { Driver Code Starts.
+int main()
+{
+    //taking testcases
+	int t;
+	cin >> t;
+	
+	while(t--)
+	{
+	    //taking number of steps in stair
+		int n;
+		cin>>n;
+		Solution ob;
+		//calling function countWays()
+		cout << ob.countWays(n) << endl;
+	}
+    
+    return 0;
+    
+}
